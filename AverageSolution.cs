11f819f1fb7d349accbe1b2eb0947b92a8fcb149
Fady Konsowa <fadykonsowa@gmail.com>
2026-03-26 07:01:13 +0200

    Add files via upload

---
+++ AverageSolution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculateAverageLib
+{
+    public class AverageSolution
+    {
+        public static double FindAverage(double[] array)
+        {
+            if (array.Length == 0) return 0;
+
+            double avg = 0;
+
+            foreach (var d in array)
+            {
+                avg += d;
+            }
+
+            return avg / array.Length;
+        }
+    }
+}
---
+++ CalculateAverage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CalculateAverageLib;
+
+class CalculateAverage
+{
+    static void Main(string[] args)
+    {
+        List<double> numbersList = new List<double>();
+
+        Console.WriteLine("Enter the numbers one by one.\nWhen you're finished, type 's' and then press Enter.\n");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+
+            // التحقق من انتهاء الإدخال
+            if (input.ToLower() == "s")
+                break;
+
+            // محاولة تحويل الإدخال إلى رقم
+            if (double.TryParse(input, out double number))
+            {
+                numbersList.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("The entered value is not a valid number, please try again.");
+            }
+        }
+
+        // تحويل القائمة إلى مصفوفة
+        double[] numbers = numbersList.ToArray();
+
+        // حساب المتوسط باستخدام المكتبة
+        double result = AverageSolution.FindAverage(numbers);
+
+        Console.WriteLine($"Average numbers\n: {result}");
+        Console.ReadKey();
+    }
+}
